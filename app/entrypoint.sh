ec056d2a9919dcba2271a58e9463e2c832c8fa51
Corentin LEVET <clevet@clevet-laptop.local>
2023-12-23 00:26:10 +0100

    [~] Changing flutter to react native

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # If an apk already exists, copy it to /apk
-if [ -f /app/build/app/outputs/apk/release/app-release.apk ]; then
-  cp /app/build/app/outputs/apk/release/app-release.apk /apk/client.apk
+if [ -f /app/build/app/outputs/apk/debug/app-debug.apk ]; then
+  cp /app/build/app/outputs/apk/debug/app-debug.apk /apk/client.apk
 fi
 
-# Build new apk
-flutter build apk
+# cd android && ./gradlew assembleDebug && cd ..
+sh -c "cd android && ./gradlew assembleDebug > /dev/null 2>&1 && cd .. && cp /app/android/app/build/outputs/apk/debug/app-debug.apk /apk/client.apk" &
 
-# Copy new apk to /apk
-cp /app/build/app/outputs/apk/release/app-release.apk /apk/client.apk
+# Run the app
+npm install --legacy-peer-deps && npx expo start --tunnel
