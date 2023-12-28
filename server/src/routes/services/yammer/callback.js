27124dd51a12abade698b12725fddbc66227f8d7
Nathn <nathan@tranchant.fr>
2023-12-28 19:21:50 +0100

    Clean server code

--- server/src/routes/services/yammer/callback.js
+++ server/src/routes/services/yammer/callback.js
@@ -1,42 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+
 const User = require("@/models/User");
 
-  const findUserInRequestCookies = async (req) => {
-    const cookiesUser = JSON.parse(req.cookies.user);
-    if (!cookiesUser) {
-      return null;
-    }
+const findUserInRequestCookies = async (req) => {
+  const cookiesUser = JSON.parse(req.cookies.user);
+  if (!cookiesUser) {
+    return null;
+  }
 
-    const user = await User.findOne({ uid: cookiesUser.uid });
-    return user;
-  };
+  const user = await User.findOne({ uid: cookiesUser.uid });
+  return user;
+};
 
-    const initUserYammerAuth = async (user) => {
-        if (!user.auth) {
-            user.auth = {
-            yammer: {},
-            };
-        }
+const initUserYammerAuth = async (user) => {
+  if (!user.auth) {
+    user.auth = {
+      yammer: {},
+    };
+  }
 
-        try {
-            await user.save();
-        } catch (error) {
-            console.log("Error saving user:", error);
-        }
-        }
+  try {
+    await user.save();
+  } catch (error) {
+    console.log("Error saving user:", error);
+  }
+};
 
 router.get("/callback", async (req, res) => {
   const code = req.query.code;
 
   try {
-    const response = await axios.post("https://www.yammer.com/oauth2/access_token", {
-      client_id: process.env.YAMMER_CLIENT_ID,
-      client_secret: process.env.YAMMER_CLIENT_SECRET,
-      code: code,
-      grant_type: "authorization_code",
-    });
+    const response = await axios.post(
+      "https://www.yammer.com/oauth2/access_token",
+      {
+        client_id: process.env.YAMMER_CLIENT_ID,
+        client_secret: process.env.YAMMER_CLIENT_SECRET,
+        code: code,
+        grant_type: "authorization_code",
+      }
+    );
 
     const accessTokens = response.data.access_token;
 
@@ -52,7 +56,7 @@ router.get("/callback", async (req, res) => {
     await user.save();
 
     // Redirect or send a response based on the success of the authentication
-    res.redirect("http://localhost:8081/new");
+    res.redirect(`${process.env.FRONT_URL}/new`);
   } catch (error) {
     // Handle errors, log them, and redirect to an error page
     console.error("Yammer authentication error:", error.message);
