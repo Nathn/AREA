c274e6c8296808fa1d875cb4c263e4ac9b4ccab5
Nathn <nathan@tranchant.fr>
2024-01-08 13:49:47 +0100

    Outlook - Not working sendMail reaction

--- server/src/routes/services/outlook/index.js
+++ server/src/routes/services/outlook/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const callback = require("./callback");
 const baseValues = require("./baseValues");
 const actions = require("./actions");
+const reactions = require("./reactions");
 
 router.get("/", async (req, res) => {
   const url = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${process.env.OUTLOOK_CLIENT_ID}&scope=https://graph.microsoft.com/.default&response_type=code&redirect_uri=${process.env.OUTLOOK_CALLBACK_URL}`;
@@ -14,5 +15,6 @@ router.get("/", async (req, res) => {
 router.use("/", callback);
 router.use("/", baseValues);
 router.use("/action", actions);
+router.use("/reaction", reactions);
 
 module.exports = router;
