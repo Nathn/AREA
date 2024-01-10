5506170feb3bd2e45cf02c07fcf0292b5bd8ed51
corentinlevet <corentin.levet@epitech.eu>
2024-01-10 17:10:19 +0100

    [+] 30 actions biatch :)

--- server/src/routes/services/github/actions/forks.js
+++ server/src/routes/services/github/actions/forks.js
@@ -2,6 +2,23 @@ const express = require("express");
 const router = express.Router();
 const GitHubApiHandler = require("../utils/GitHubApiHandler");
 
+const handleForkRepo = async (userForks, accessToken, action) => {
+  const prevForkSize = userForks.map((repository) => repository.length).reduce((a, b) => a + b, 0);
+
+  const githubApiHandler = new GitHubApiHandler(accessToken);
+  const userPublicRepositories = await githubApiHandler.getPublicRepositories();
+  const newUserForks = await githubApiHandler.getForksForPublicRepositories(userPublicRepositories);
+
+  const newForkSize = newUserForks.map((repository) => repository.length).reduce((a, b) => a + b, 0);
+
+  return {
+    result: action === "add" ? newForkSize > prevForkSize : action === "remove" ? newForkSize < prevForkSize : false,
+    newBaseValues: newUserForks,
+    baseValuesId: "forks",
+    reactionNeededBaseValues: null,
+  };
+}
+
 router.post("/forkRepo", async (req, res) => {
   const { user, baseValues } = req.body;
   const userForks = baseValues?.forks;
@@ -13,20 +30,27 @@ router.post("/forkRepo", async (req, res) => {
     return;
   }
   try {
-    const prevForkSize = userForks.map((repository) => repository.length).reduce((a, b) => a + b, 0);
+    const result = await handleForkRepo(userForks, accessToken, "add");
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Bad request");
+  }
+});
 
-    const githubApiHandler = new GitHubApiHandler(accessToken);
-    const userPublicRepositories = await githubApiHandler.getPublicRepositories();
-    const newUserForks = await githubApiHandler.getForksForPublicRepositories(userPublicRepositories);
+router.post("/unforkRepo", async (req, res) => {
+  const { user, baseValues } = req.body;
+  const userForks = baseValues?.forks;
 
-    const newForkSize = newUserForks.map((repository) => repository.length).reduce((a, b) => a + b, 0);
+  const accessToken = user?.auth?.github?.access_token;
 
-    res.status(200).send({
-      result: newForkSize > prevForkSize,
-      newBaseValues: newUserForks,
-      baseValuesId: "forks",
-      reactionNeededBaseValues: null,
-    });
+  if (!user || !userForks || !accessToken) {
+    res.status(400).send("Bad request");
+    return;
+  }
+  try {
+    const result = await handleForkRepo(userForks, accessToken, "remove");
+    res.status(200).send(result);
   } catch (error) {
     console.log(error);
     res.status(400).send("Bad request");
