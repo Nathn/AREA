a7d167ab306a367579a595e96b86ec2b19662bf9
Hugo GRISEL <hugo.grisel@epitech.eu>
2024-01-15 09:57:56 +0100

    (~] remove unuse css

--- web/src/pages/Client/index.jsx
+++ web/src/pages/Client/index.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 
-import "./index.css";
-
 function App({ user, services }) {
   useEffect(() => {
     window.location.assign("http://172.18.0.4:8090/client.apk")
